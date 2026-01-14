9fea955b30482d8de4971ab152156c40ccdabdd9
krisschaaf <kris-schaaf@gmx.de>
2026-01-14 21:27:12 +0100

    test: make github workflow fail on purpose

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class MainTest {
+    @Test
+    public void failOnPurpose() {
+        fail("Failing on Purpose!");
+    }
+}
